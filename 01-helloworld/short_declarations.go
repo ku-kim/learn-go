a66c7acc1525d8b8bf30927e758c810b9203c1a6
“kukim” <kukim.dev@gmail.com>
2023-03-05 19:14:08 +0900

    1: variable(short 할당, var)

---
+++ 01-helloworld/short_declarations.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	fmt.Println(x)
+
+	name := "kukim"
+	fmt.Println(name)
+
+	// var
+	var name2 = "kukim2"
+	var value int = 1
+	fmt.Println(name2)
+	fmt.Println(value)
+}
