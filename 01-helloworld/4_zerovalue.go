f7347d75734320e659f132d3623840e4468c96eb
“kukim” <kukim.dev@gmail.com>
2023-03-05 19:25:54 +0900

    1: zero value(기본값, nil)

---
+++ 01-helloworld/4_zerovalue.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var x int     // 0
+	var y string  // ""
+	var z float64 // 0.0
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+
+	// nil
+	// pointers, functions, interfaces, slices, channels, maps
+}
