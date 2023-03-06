8923642b4fc588e4a9939f0f20d52d41f3f68c15
“kukim” <kukim.dev@gmail.com>
2023-03-06 13:55:42 +0900

    7: method sets & ex

--- 07-pointer/3_method_sets.go
+++ 07-pointer/3_method_sets.go
@@ -1,1 +1,17 @@
 package main
+
+/*
+
+● NON-POINTER RECEIVER & NON-POINTER VALUE
+○ https://play.golang.org/p/2ZU0QX12a8
+● NON-POINTER RECEIVER & POINTER VALUE
+○ https://play.golang.org/p/glWZmm0gY6
+● POINTER RECEIVER & POINTER VALUE
+○ https://play.golang.org/p/pWFxsg6MSe
+● POINTER RECEIVER & NON-POINTER VALUE
+○ https://play.golang.org/p/G3lEy-4Mc8 ( code does not run )
+○ this codes does run - notice the difference - method set determines
+the INTERFACES that the type implements
+■ https://play.golang.org/p/KK8gjsAWBZ
+
+*/
