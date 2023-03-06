0262b1ff2f923f9e0a9adbc1577732440b95fa4e
“kukim” <kukim.dev@gmail.com>
2023-03-06 11:46:24 +0900

    6: anonymous func

---
+++ 06_functions/7_anonymous_func.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// 익명함수, 함수 이름 없음
+func main() {
+
+	func() {
+		fmt.Println("Anonymous Func ran")
+	}()
+
+	func(x int) {
+		fmt.Println("Anonymous Func ran", x)
+	}(42)
+
+	f := func(str string) {
+		fmt.Println("Anonymous Func ran", str)
+	}
+	f("haha")
+}
