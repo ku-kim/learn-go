b57d51f60394e564a8a3dff8286df443f3b5d2a6
kukim <kukim.kim@groundx.xyz>
2023-03-07 09:25:17 +0900

    10: comma ok pattern

--- 10-channel/6_commaok_pattern.go
+++ 10-channel/6_commaok_pattern.go
@@ -2,11 +2,10 @@ package main
 
 import "fmt"
 
-// https://velog.io/@moonyoung/golang-channel-with-select-%ED%97%B7%EA%B0%88%EB%A6%AC%EB%8A%94-%EC%BC%80%EC%9D%B4%EC%8A%A4-%EC%A0%95%EB%A6%AC%ED%95%98%EA%B8%B0
 func main() {
 	eve := make(chan int)
 	odd := make(chan int)
-	quit := make(chan int)
+	quit := make(chan bool)
 
 	// send
 	go send(eve, odd, quit)
@@ -15,7 +14,7 @@ func main() {
 
 }
 
-func send(e, o, q chan<- int) {
+func send(e, o chan<- int, q chan<- bool) {
 	for i := 0; i < 100; i++ {
 		if i%2 == 0 {
 			e <- i
@@ -23,21 +22,25 @@ func send(e, o, q chan<- int) {
 			o <- i
 		}
 	}
+	close(q)
 	// close(e)
 	// close(o)
-	q <- 0
 }
 
-func receive(e, o, q <-chan int) {
+func receive(e, o <-chan int, q <-chan bool) {
 	for {
 		select {
 		case v := <-e:
 			fmt.Println("from eve channel: ", v)
 		case v := <-o:
 			fmt.Println("from odd channel: ", v)
-		case v := <-q:
-			fmt.Println("from quit channel: ", v)
-			return
+		case i, ok := <-q:
+			if !ok {
+				fmt.Println("from comma ok : ", i, ok) // i: value(false), ok : 채널 닫혀있는지 여부(닫혀있으면 false, 열려있으면 true)
+				return
+			} else {
+				fmt.Println("from comma ok: ", i)
+			}
 		}
 	}
 }
