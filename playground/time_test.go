95e97d6b8777b147db5f7fae13a361b7dc2999f7
kukim <kukim.kim@groundx.xyz>
2023-03-22 18:12:34 +0900

    playground: 오늘 날짜 기준 string 2023-03-21 00:00:00 +0900 KS 뽑기

--- playground/time_test.go
+++ playground/time_test.go
@@ -6,9 +6,23 @@ import (
 	"time"
 )
 
-func TestTimemethod(t *testing.T) {
+func TestTimeMethod(t *testing.T) {
 	// 현재날짜 -8d
 	y, m, d := time.Now().AddDate(0, 0, -8).UTC().Date()
 	fmt.Println(fmt.Sprintf("%d-%d-%d", y, m, d)) // 2023-3-9
 
+}
+
+func TestBaseTime(t *testing.T) {
+	t1 := baseTime(time.Now())
+
+	fmt.Println(t1) // 2023-03-21 00:00:00 +0900 KST, 함수 시작하는 날짜 00:00:00
+	fmt.Println(time.Now())
+}
+
+func baseTime(t time.Time) time.Time {
+	loc, _ := time.LoadLocation("Asia/Seoul")
+	year, month, day := t.In(loc).Date()
+	onTime := time.Date(year, month, day, 0, 0, 0, 0, loc)
+	return onTime
 }
